d7da5bd194b1198137a5c6e906a8b1486b62542f
minthukha <minthukha1239788@gmail.com>
2024-04-30 16:21:29 +0630

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+string ExtractTextFromHtml(string html)
+{
+    if (html == null)
+    {
+        return "null";
+    }
+
+    string plainText = Regex.Replace(html, "<[^>]+?>", " ");
+    plainText = System.Net.WebUtility.HtmlDecode(plainText).Trim();
+
+    return plainText;
+}
+
+string htmlContent = "<p>Hello World</p>";
+string text = ExtractTextFromHtml(htmlContent);
+Console.WriteLine(text);
